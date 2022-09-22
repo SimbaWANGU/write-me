400b0a19e7ae1b8914985e3cc283ccb856582eb8
SimbaWANGU <simbawangulu@gmail.com>
2022-09-22 11:20:30 +0300

    add react router dom to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import LoginRegister from "./pages/loginRegister";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import PublicPosts from "./pages/public";
+import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-      <LoginRegister />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<PublicPosts />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
