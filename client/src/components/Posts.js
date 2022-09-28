dd823caee81b795035758e43bc8130bca304a135
SimbaWANGU <simbawangulu@gmail.com>
2022-09-28 17:14:14 +0300

    add post methods

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,12 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Posts.scss'
 import CommentIcon from '@mui/icons-material/Comment';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 function Posts(props) {
+  const [username, setUsername] = useState('')
   let image = ''
   let pi = (image === '') ? 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' : image;
+
+  React.useEffect(() => {
+    const loggedInUser = JSON.parse(localStorage.getItem('write-me-user'))
+    setUsername(loggedInUser.username)
+  }, [])
+
+  const getLikes = async () => {
+    const res = await fetch(`/post/${props.id}/likes`, {
+      method: 'get',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      },
+    })
+    const result = await res.json()
+    return result
+  }
+
+  const getComments = async () => {
+    const res = await fetch(`/post/${props.id}/comments`, {
+      method: 'get',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      },
+    })
+    const result = await res.json()
+    return result
+  }
+
+  const likePost = async () => {
+    await fetch('/post/like', {
+      method: 'post',
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded"
+      },
+      body: `username=${username}&id=${props.id}`
+    })
+    .then(res => res.json())
+    .then((data) => console.log(data))
+  }
+
+  const unlikePost = async () => {}
+
+  const useAddLikeData = () => {
+    const queryClient = useQueryClient()
+    return useMutation(likePost, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(`likes${props.id}`)
+      }
+    })
+  }
+
+  const { mutate: addLikeToDb } = useAddLikeData()
+  const {data: likesData} = useQuery(`likes${props.id}`, getLikes)
+  const {data: commentsData} = useQuery(`comments${props.id}`, getComments)
+
+  const handleLike = () => {
+    const like = {
+      username,
+      id: props.id
+    }
+    addLikeToDb(like)
+  }
+
+  const LikeButton = () => {
+    if(likesData) {
+      if (likesData.likes.includes(username)) {
+        return <FavoriteIcon className='icon' color='error' />
+      } else {
+        return <FavoriteBorderIcon className='icon' onClick={handleLike} />
+      }
+    }
+  }
 
   return (
     <div className='post'>
@@ -16,8 +90,8 @@ function Posts(props) {
       </div>
         {props.body}
       <div className='post_icons'>
-        {props.comments.length} <CommentIcon className='icon' color='primary' />
-        {props.likes.length}<FavoriteIcon className='icon' color='error' />
+        {(commentsData) ? commentsData.comments.length : 0} <CommentIcon className='icon' color='primary' />
+        {(likesData) ? likesData.likes.length : 0} <LikeButton />
       </div>
     </div>
   )
